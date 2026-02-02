64373dc0cbddd4da26cabcab015b7b8908143371
robertfabio <fabio.filho25453@alunos.ufersa.edu.br>
2026-02-02 09:34:54 -0300

    remove commented-out code for compra creation and display

--- main.go
+++ main.go
@@ -8,24 +8,5 @@ import (
 
 func main() {
 
-	var itemNames []string
-
-	itemNames = append(itemNames, "Arroz", "Feijão", "Macarrão")
 	
-	compra, err := model.NewCompra("Mercadinho", time.Now(), itemNames)
-	
-	if err != nil {
-		fmt.Println("Error creating compra:", err)
-		return
-	} else {
-
-	fmt.Println("Compra Details:")
-	fmt.Println("Market:", compra.Market)
-	fmt.Println("Date:", compra.Data.Format("2006-01-02"))
-	fmt.Println("Items Bought:")
-	
-	for _, item := range compra.ItemBuys {
-		fmt.Println("-", item.ItemName)
-		}
-	}
 }
--- model/compras.go
+++ model/compras.go
@@ -37,3 +37,26 @@ func NewCompra(market string, data time.Time, itemNames []string) (*Compra, erro
 		ItemBuys: itens,
 	}, nil
 }
+
+
+{/*
+
+	itemNames = append(itemNames, "Arroz", "Feijão", "Macarrão")
+	
+	compra, err := model.NewCompra("Mercadinho", time.Now(), itemNames)
+	
+	if err != nil {
+		fmt.Println("Error creating compra:", err)
+		return
+	} else {
+
+	fmt.Println("Compra Details:")
+	fmt.Println("Market:", compra.Market)
+	fmt.Println("Date:", compra.Data.Format("2006-01-02"))
+	fmt.Println("Items Bought:")
+	
+	for _, item := range compra.ItemBuys {
+		fmt.Println("-", item.ItemName)
+		}
+	}
+*/}
