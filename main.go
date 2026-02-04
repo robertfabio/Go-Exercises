5afda3d88530c3aba92eb85069ec8131d05520ea
robertfabio <fabio.filho25453@alunos.ufersa.edu.br>
2026-02-04 15:44:51 -0300

    refactor: add mutex for safe concurrent increment of counter

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
+import (
+	"sync"
+	"time"
+)
+
 func main() {
+	var fabio sync.Mutex
 	i := 0
 	for j := 0; j < 10000; j++ {
 		go func() {
+			fabio.Lock()
 			i++
+			fabio.Unlock()
 		}()
 	}
+	time.Sleep(time.Second * 2)
 	println(i)
 }
 
